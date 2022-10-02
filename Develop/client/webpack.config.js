2ebfb8575c3784846180155726bddc0b9ed70ab8
Joseph <joelanese8@gmail.com>
2022-10-02 17:21:35 -0400

    updated code

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,13 +3,15 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
     mode: 'development',
     entry: {
       main: './src/js/index.js',
-      install: './src/js/install.js',
-      //database: './src/js/database.js'
+      install: './src/js/install.js'
     },
     output: {
       filename: '[name].bundle.js',
